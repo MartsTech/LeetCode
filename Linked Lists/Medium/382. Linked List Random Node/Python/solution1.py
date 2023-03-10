ecec4fc6362c229919eaa8dd986dc41d2c43adb6
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-03-10 18:55:17 +0200

    Add 382. Linked List Random Node

---
+++ Linked Lists/Medium/382. Linked List Random Node/Python/solution1.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def __init__(self, head: Optional[ListNode]):
+        self.head = head
+
+    # O(n) time | O(1) space
+    def getRandom(self) -> int:
+        sample = self.head
+        current = self.head
+        count = 1
+        while current:
+            if random.randint(1, count) == 1:
+                sample = current
+            current = current.next
+            count += 1
+        return sample.val
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
---
+++ Linked Lists/Medium/382. Linked List Random Node/README.md
@@ -0,0 +1,3 @@
+[Problem](https://leetcode.com/problems/linked-list-random-node)
+
+[Solution](https://leetcode.com/problems/linked-list-random-node/solutions/3280961/382-linked-list-random-node-simple-solution)
