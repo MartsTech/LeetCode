f179ae5e5c26ddf549eb6b3d5c4c61ddf2a6f9c4
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-03-14 19:58:53 +0200

    Add 387. First Unique Character in a String~

---
+++ Strings/Easy/387. First Unique Character in a String/Python/solution1.py
@@ -0,0 +1,10 @@
+class Solution:
+    # O(n) time | O(n) space
+    def firstUniqChar(self, s: str) -> int:
+        freq = {}
+        for c in s:
+            freq[c] = freq.get(c, 0) + 1
+        for i in range(len(s)):
+            if freq[s[i]] == 1:
+                return i
+        return -1
---
+++ Strings/Easy/387. First Unique Character in a String/README.md
@@ -0,0 +1,3 @@
+[Problem](https://leetcode.com/problems/first-unique-character-in-a-string)
+
+[Solution](https://leetcode.com/problems/first-unique-character-in-a-string/solutions/3297414/387-first-unique-character-in-a-string-simple-solution)
