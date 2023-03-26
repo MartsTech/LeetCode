a77937f8e7971913b258eb35a3ebe409e93442a2
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-03-26 11:26:57 +0300

    Add 344. Reverse String

---
+++ Strings/Easy/344. Reverse String/Python/solution1.py
@@ -0,0 +1,8 @@
+class Solution:
+    # O(n) time | O(1) space
+    def reverseString(self, s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
---
+++ Strings/Easy/344. Reverse String/README.md
@@ -0,0 +1,3 @@
+[Problem](https://leetcode.com/problems/reverse-string)
+
+[Solution](https://leetcode.com/problems/reverse-string/solutions/3342932/344-reverse-string-simple-solution)
