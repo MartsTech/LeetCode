66c75f0c4ebf2a3972afa58b6b157f60b5f34754
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-06-28 14:05:15 +0300

    49. Group Anagrams

---
+++ Strings & Hashing/49. Group Anagrams/Python/solution1.py
@@ -0,0 +1,7 @@
+class Solution:
+    # O(n) time | O(n) space
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for s in strs:
+            groups[''.join(sorted(s))].append(s)
+        return groups.values()
---
+++ Strings & Hashing/49. Group Anagrams/README.md
@@ -0,0 +1,3 @@
+[Problem](https://leetcode.com/problems/group-anagrams)
+
+[Solution](https://leetcode.com/problems/group-anagrams/solutions/3658046/49-group-anagrams-simple-solution)
