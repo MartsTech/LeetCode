ef0b6a20b581b61f42824f8fe2e9c4b0f082552d
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-07-27 22:26:11 +0300

    13. Roman to Integer

---
+++ Arrays & Hashing/Easy/13. Roman to Integer/Python/solution1.py
@@ -0,0 +1,11 @@
+class Solution:
+    # O(n) time | O(1) space
+    def romanToInt(self, s: str) -> int:
+        values = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
+        res = values[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
+            if values[s[i]] < values[s[i + 1]]:
+                res -= values[s[i]]
+            else:
+                res += values[s[i]]
+        return res
