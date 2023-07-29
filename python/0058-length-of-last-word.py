5a766d8fe2fada2e2d590860e26f256e17895adc
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-07-29 20:14:03 +0300

    58. Length of Last Word

---
+++ python/0058-length-of-last-word.py
@@ -0,0 +1,10 @@
+class Solution:
+    # O(n) time | O(1) space
+    def lengthOfLastWord(self, s: str) -> int:
+        i, count = len(s) - 1, 0
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            count += 1
+            i -= 1
+        return count
