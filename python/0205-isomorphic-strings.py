798f954f4c1f7299bc87e4a4c5fb759ee548fc6d
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-07-31 00:56:10 +0300

    205. Isomorphic Strings

---
+++ python/0205-isomorphic-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    # O(n) time | O(n) space
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_map, t_map = {}, {}
+        for i in range(len(s)):
+            if s[i] in s_map and s_map[s[i]] != t[i]:
+                return False
+            if t[i] in t_map and t_map[t[i]] != s[i]:
+                return False
+            s_map[s[i]], t_map[t[i]] = t[i], s[i]
+        return True
