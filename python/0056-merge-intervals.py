834ed88a650e858d4764ba806c2c3a0750a1b669
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-07-31 22:29:42 +0300

    56. Merge Intervals

---
+++ python/0056-merge-intervals.py
@@ -0,0 +1,11 @@
+class Solution:
+    # O(n*log(n)) time | O(n) space
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda i: i[0])
+        res = [intervals[0]]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= res[-1][1]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+            else:
+                res.append(intervals[i])
+        return res
