ec7381e935ebe6b2538e1ab2f5f1db5699ef2290
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-08-01 21:46:21 +0300

    141. Linked List Cycle

---
+++ python/0141-linked-list-cycle.py
@@ -0,0 +1,11 @@
+class Solution:
+    # O(n) time | O(1) space
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+                
