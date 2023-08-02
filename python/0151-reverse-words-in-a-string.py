774be95112ce7dfbb7038b0c274c9c047af23304
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-08-02 16:12:32 +0300

    151. Reverse Words in a String

--- python/0151-reverse-words-in-a-string.py
+++ python/0151-reverse-words-in-a-string.py
@@ -1,17 +1,14 @@
 class Solution:
-    # O(n) time | O(n) spacec
+    # O(n) time | O(n) space - where n is the length of s
     def reverseWords(self, s: str) -> str:
-        r, res = len(s) - 1, []
+        res, r = [], len(s) - 1
         while r >= 0:
-            if s[r] == ' ':
+            if s[r] == " ":
                 r -= 1
                 continue
             l = r - 1
-            while l >= 0 and s[l] != ' ':
+            while l >= 0 and s[l] != " ":
                 l -= 1
-            for i in range(l + 1, r + 1):
-                res += s[i]
-            r = l
-            res += ' '
-        res.pop() # removes extra space
-        return ''.join(res)
+            res.append(s[l + 1: r + 1])
+            r = l - 1
+        return " ".join(res)
