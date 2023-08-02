bd126c8f670a14009df88a8b12bd2eabb2c4b429
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-08-02 16:27:56 +0300

    6. Zigzag Conversion

--- python/0006-zigzag-conversion.py
+++ python/0006-zigzag-conversion.py
@@ -1,15 +1,14 @@
 class Solution:
-    # O(n) time | O(n) space
+    # O(n) time | O(n) space - where n is the lenght of s
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1: return s
-        res = []
-        bigStep = 2 * (numRows - 1)
-        for row in range(numRows):
-            for i in range(row, len(s), bigStep):
+        res, bigStep = [], 2 * (numRows - 1)
+        for r in range(numRows):
+            for i in range(r, len(s), bigStep):
                 res.append(s[i])
-                if row == 0 or row == numRows - 1:
+                if r == 0 or r == numRows - 1:
                     continue
-                smallStep = bigStep - 2 * row
+                smallStep = bigStep - 2 * r
                 if i + smallStep < len(s):
                     res.append(s[i + smallStep])
-        return ''.join(res)
+        return "".join(res)
