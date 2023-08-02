76a5f4139cf2638d61e31ed2941b628fcd7d1294
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-08-02 17:15:30 +0300

    68. Text Justification

--- python/0068-text-justification.py
+++ python/0068-text-justification.py
@@ -1,30 +1,33 @@
 class Solution:
+    # O(n) time | O(n) space - where n is the length of words
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         res, idx = [], 0
         while idx < len(words):
-            line, length = self.get_words(idx, words, maxWidth)
+            line, length = self.getWords(idx, words, maxWidth)
             idx += len(line)
-            res.append(self.create_line(line, length, idx, words, maxWidth))
+            res.append(self.buildLine(idx, line, length, words, maxWidth))
         return res
 
-    def get_words(self, idx: int, words: List[str], maxWidth: int) -> Tuple[List[str], int]:
+    # O(n) time | O(n) space
+    def getWords(self, idx: int, words: List[int], maxWidth: int) -> Tuple[List[int], int]:
         line, length = [], 0
         while idx < len(words) and length + len(words[idx]) <= maxWidth:
             line.append(words[idx])
             length += len(words[idx]) + 1
             idx += 1
-        length -= 1
+        lenght -= 1
         return (line, length)
 
-    def create_line(self, line: List[str], length: int, idx: int, words: List[str], maxWidth: int) -> str:
+    # O(n) time | O(1) space
+    def buildLine(self, idx: int, line: List[int], length: int, words: List[int], maxWidth: int) -> str:
         spaces = maxWidth - length
         if len(line) == 1 or idx == len(words):
-            return ' '.join(line) + ' ' * spaces
-        word_count = len(line) - 1
-        spaces_per_word = spaces // word_count
-        extra_space = spaces % word_count
-        for i in range(extra_space):
-            line[i] += ' '
-        for i in range(word_count):
-            line[i] += ' ' * spaces_per_word
-        return ' '.join(line)
+            return " ".join(line) + " " * spaces
+        words_count = len(line) - 1
+        spaces_per_word = spaces // words_count
+        for i in range(words_count):
+            line[i] += " " * spaces_per_word
+        extra_spaces = spaces % words_count
+        for i in range(extra_spaces):
+            line[i] += " "
+        return " ".join(line)
