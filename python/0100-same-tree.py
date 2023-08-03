21c45ad3be231185cead1a57e77a18032d5d5784
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-08-03 16:44:17 +0300

    100. Same Tree

---
+++ python/0100-same-tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    # O(n) time | O(d) space
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        if not self.isSameTree(p.left, q.left):
+            return False
+        if not self.isSameTree(p.right, q.right):
+            return False
+        return True
