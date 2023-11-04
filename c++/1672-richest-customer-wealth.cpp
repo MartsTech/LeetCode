66745a4e8bcca40551edf797c645c45ebc27f02c
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-11-04 23:41:21 +0200

    1672. Richest Customer Wealth

--- c++/1672-richest-customer-wealth.cpp
+++ c++/1672-richest-customer-wealth.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    // O(m*n) time | O(1) space - m is the number of customers, n is the number of banks
+    // O(m*n) time, O(1) space
     int maximumWealth(vector<vector<int>>& accounts) {
-        int res = 0;
-        int m = accounts.size();
-        int n = accounts[0].size();
-        for (int i = 0; i < m; ++i) {
+        int max_wealth = 0;
+        size_t m = accounts.size(), n = accounts[0].size();
+        for (size_t i = 0; i < m; ++i) {
             int wealth = 0;
-            for (int j = 0; j < n; ++j) {
+            for (size_t j = 0; j < n; ++j) {
                 wealth += accounts[i][j];
             }
-            res = max(res, wealth);
+            max_wealth = max(max_wealth, wealth);
         }
-        return res;
+        return max_wealth;
     }
 };
