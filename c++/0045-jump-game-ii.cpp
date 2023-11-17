9e459df2ce44879bdec3d9283db24cf044132f8b
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-11-17 21:28:57 +0200

    45. Jump Game II

--- c++/0045-jump-game-ii.cpp
+++ c++/0045-jump-game-ii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    // O(n) time, O(1) space - where n is the size of the input array
+    // O(n) time | O(1) space
     int jump(vector<int>& nums) {
-        int res = 0;
+        int minJumps = 0;
         int l = 0, r = 0;
         int n = nums.size();
         while (r < n - 1) {
@@ -12,8 +12,8 @@ public:
             }
             l = r + 1;
             r = jump;
-            ++res;
+            ++minJumps;
         }
-        return res;
+        return minJumps;
     }
 };
