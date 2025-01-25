56506a3fb741afb45aa7a4ec6485fcee92d3c644
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2025-01-25 11:22:50 +0200

    Time: 15 ms (11.14%) | Memory: 12.3 MB (71.57%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    std::unordered_map<char, int> map = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
     // O(n) time | O(1) space
     int romanToInt(string s) {
-        unordered_map<char, int> map = {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
         int res = 0, n = s.size();
         for (int i = 0; i < n; ++i) {
             if (i + 1 < n && map[s[i]] < map[s[i + 1]]) {
