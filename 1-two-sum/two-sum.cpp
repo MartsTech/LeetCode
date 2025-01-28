c0f068642b0b7424c1953d8d3942dca25e11c196
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2025-01-28 15:24:45 +0200

    Time: 1 ms (83.01%) | Memory: 14.7 MB (54.36%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     // O(n) time | O(n) space
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> map;
+        unordered_map<int, int> map;
         for (int i = 0; i < nums.size(); ++i) {
             if (map.find(target - nums[i]) != map.end()) {
                 return {i, map[target - nums[i]]};
