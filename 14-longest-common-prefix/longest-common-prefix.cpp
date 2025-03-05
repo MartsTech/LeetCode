a1a2ff5350024ac99353aa007aa6aaca8ec5a69b
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2025-03-05 11:29:31 +0200

    Time: 0 ms (100.00%) | Memory: 11.8 MB (70.00%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    // 0(m * n log n) | O(1) space
+    // O(m * n log n) time | O(1) space
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.size() == 0) return "";
         sort(strs.begin(), strs.end());
         string& first = strs[0], last = strs.back();
         int i = 0;
-        while (i < first.size() && i < last.size() && first[i] == last[i]) ++i;
+        while (i < min(first.size(), last.size()) && first[i] == last[i]) ++i;
         return first.substr(0, i);
     }
 };
